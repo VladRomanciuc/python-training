6ec0746b0f3293fbe5d2b9ecc2bc19eede1c2100
Vlad Romanciuc <vladislavromanciuc@gmail.com>
2022-02-18 10:36:17 +0000

    to master

--- SQL/explore.sql
+++ SQL/explore.sql
@@ -15,6 +15,19 @@ FROM pub1.titles
 WHERE(lower(bktitle) NOT LIKE "%repair%" AND lower(bktitle) NOT LIKE "%play%" 
 AND lower(bktitle) NOT LIKE "%build%");
 
+/*query creates a transaction to extract titles older than 33 months into an obsolete table and removes them from initial table*/
+/*DELETE FROM pub1.titles WHERE partnum="39905";*/
+START TRANSACTION;
+SET SQL_SAFE_UPDATES = 0;
+INSERT INTO pub1.obsolete_titles
+    SELECT * FROM pub1.titles
+    WHERE pubdate < DATE_SUB(NOW(), INTERBAL 33 MONTH);
+DELETE FROM pub1.titles
+    WHERE pubdate < DATE_SUB(NOW(), INTERBAL 33 MONTH);
+SET SQL_SAFE_UPDATES = 1;
+COMMIT;
+
+
 /*query returns the total sold for the items with the given part number*/
 SELECT SUM(qty) FROM pub1.sales WHERE partnum = "40121";
 SELECT SUM(qty) FROM pub1.sales WHERE partnum = "40125";
@@ -30,3 +43,16 @@ SELECT partnum, bktitle, slprice, pubdateFROM pub1.titles
 FROM pub1.titles
 WHERE bktitle LIKE "% % %"
 ORDER BY slprice DESC LIMIT 20;
+
+/*query returns a list of sales persons by the quantity of products they sold*/
+SELECT slspers.repid as rep,
+CONCAT(TRIM(lname), ", ", TRIM(fname)) as fullname,
+SUM(sales.qty) as quantity
+FROM pub1.sales
+LEFT JOIN pub1.slspers ON (pub1.sales.repid = pub1.slspers.repid)
+GROUP BY slspers.repid
+ORDER BY quantity DESC;
+
+
+/*query delete the inventory table*/
+DROP TABLE IF EXISTS pub1.inventory;
